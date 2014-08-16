db9175dab36e1349f11393af5dfb472b1bcc9b42
Josh Sixsmith <josh.sixsmith@gmail.com>
2014-08-17 09:18:16 +1000

    Changed version to 0.1, in prep for first release.

--- Lib/__init__.py
+++ Lib/__init__.py
@@ -4,4 +4,4 @@ from IDL_hist_equal import hist_equal
 from IDL_array_indices import array_indices
 from IDL_region_grow import region_grow
 from IDL_label_region import label_region
-__version__ = '1.0'
+__version__ = '0.1'
--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ extra_compile_args=['--fcompiler=gnu95']
     
 ## setup the python module
 setup(name="IDL_functions", # name of the package to import later
-      version='1.0',
+      version='0.1',
       author='Josh Sixsmith',
       author_email='joshua.sixsmith@ga.gov.au, josh.sixsmith@gmail.com',
       # Build fortran wrappers, uses f2py
