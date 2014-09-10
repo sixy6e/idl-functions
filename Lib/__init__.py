65450419288c35d14d331781360d4f58b69ffecc
Josh Sixsmith <josh.sixsmith@gmail.com>
2014-09-10 21:59:54 +1000

    Added randomu to the __init__.py imports

--- Lib/__init__.py
+++ Lib/__init__.py
@@ -4,4 +4,5 @@ from IDL_hist_equal import hist_equal
 from IDL_array_indices import array_indices
 from IDL_region_grow import region_grow
 from IDL_label_region import label_region
+from IDL_randomu import randomu
 __version__ = '0.1'
