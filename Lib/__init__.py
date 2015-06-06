5ec8429adadf0ad932f99917585d397ae87f3d4d
Josh Sixsmith <josh.sixsmith@gmail.com>
2015-06-06 15:48:05 +1000

    Namespace shanges.

--- Lib/__init__.py
+++ Lib/__init__.py
@@ -1,8 +1,8 @@
-from IDL_histogram import histogram
-from IDL_bytscl import bytscl
-from IDL_hist_equal import hist_equal
-from IDL_array_indices import array_indices
-from IDL_region_grow import region_grow
-from IDL_label_region import label_region
-from IDL_randomu import randomu
+from idl_histogram import histogram
+from idl_bytscl import bytscl
+from idl_hist_equal import hist_equal
+from idl_array_indices import array_indices
+from idl_region_grow import region_grow
+from idl_label_region import label_region
+from idl_randomu import randomu
 __version__ = '0.1'
