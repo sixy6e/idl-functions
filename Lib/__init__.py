d9fb48f8cc21c4f8a09fa2097fabdab41ee7c4dc
Josh Sixsmith <josh.sixsmith@gmail.com>
2015-06-06 21:52:22 +1000

    Re-ordered imports.

--- Lib/__init__.py
+++ Lib/__init__.py
@@ -2,7 +2,7 @@ from idl_histogram import histogram
 from idl_bytscl import bytscl
 from idl_hist_equal import hist_equal
 from idl_array_indices import array_indices
-from idl_region_grow import region_grow
 from idl_label_region import label_region
+from idl_region_grow import region_grow
 from idl_randomu import randomu
 __version__ = '0.3'
