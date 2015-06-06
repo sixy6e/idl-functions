84d36e2c59161d8d2df060c7cc074fc415a88eba
Josh Sixsmith <josh.sixsmith@gmail.com>
2015-06-06 21:10:56 +1000

    Changed test.py to account for filename changes.

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ import sys
 import subprocess
 import fnmatch
 
+
 def locate(pattern, root):
     matches = []
     for path, dirs, files in os.walk(os.path.abspath(root)):
@@ -12,23 +13,27 @@ def locate(pattern, root):
             matches.append(os.path.join(path, filename))
     return matches
 
+
 if __name__ == '__main__':
     dir_list = os.listdir(os.getcwd())
     if 'build' not in dir_list:
-        print 'The build directory was not found. Please build the IDL_fucntions module first. Exiting...'
+        msg = ("The build directory was not found. Please build the "
+               "idl_functions module first. Exiting...")
+        sys.exit(msg)
     else:
         # Change to the build directory
         os.chdir('build')
 
         # Find the unittest script
-        test_file1 = locate('unit_test_IDL_histogram.py', os.getcwd())[0]
-        test_file2 = locate('unit_test_IDL_hist_equal.py', os.getcwd())[0]
-        test_file3 = locate('unit_test_IDL_array_indices.py', os.getcwd())[0]
-        test_file4 = locate('unit_test_IDL_bytscl.py', os.getcwd())[0]
-        test_file5 = locate('unit_test_IDL_region_grow.py', os.getcwd())[0]
-        test_file6 = locate('unit_test_IDL_randomu.py', os.getcwd())[0]
+        test_file1 = locate('unit_test_idl_histogram.py', os.getcwd())[0]
+        test_file2 = locate('unit_test_idl_hist_equal.py', os.getcwd())[0]
+        test_file3 = locate('unit_test_idl_array_indices.py', os.getcwd())[0]
+        test_file4 = locate('unit_test_idl_bytscl.py', os.getcwd())[0]
+        test_file5 = locate('unit_test_idl_region_grow.py', os.getcwd())[0]
+        test_file6 = locate('unit_test_idl_randomu.py', os.getcwd())[0]
 
-        # Get the directory path that contains the unittest script and change to that directory
+        # Get the directory path that contains the unittest script and change
+        # to that directory
         dname = os.path.dirname(test_file1)
         os.chdir(dname)
 
@@ -37,16 +42,15 @@ if __name__ == '__main__':
         os.chdir(os.pardir)
 
         # Now execute the unittest script from the command line
-        print "Testing IDL_histogram"
+        print "Testing idl_histogram"
         subprocess.call(['python', test_file1])
-        print "Testing IDL_hist_equal"
+        print "Testing idl_hist_equal"
         subprocess.call(['python', test_file2])
-        print "Testing IDL_array_indices"
+        print "Testing idl_array_indices"
         subprocess.call(['python', test_file3])
-        print "Testing IDL_bytscl"
+        print "Testing idl_bytscl"
         subprocess.call(['python', test_file4])
-        print "Testing IDL_region_grow"
+        print "Testing idl_region_grow"
         subprocess.call(['python', test_file5])
-        print "Testing IDL_randomu"
+        print "Testing idl_randomu"
         subprocess.call(['python', test_file6])
-
