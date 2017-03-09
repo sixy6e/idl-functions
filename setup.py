7de210d4208ff9d93b0268a81a0ae6cdbed43997
Jeremy Hooke <jez@stulk.com>
2017-03-09 11:58:33 +1100

    Remove test_suite option from setup()
    It throws warnings on install as it appears to be incorrect. Tests are currently run from test.py instead.

--- setup.py
+++ setup.py
@@ -50,9 +50,4 @@ setup(name="idl_functions", # name of the package to import later
      ## Install these to their own directory
      package_dir = {'idl_functions':'Lib', 'idl_functions/tests':'tests'},
      packages = ["idl_functions", 'idl_functions/tests'],
-     test_suite = ['idl_functions.tests.unit_test_idl_histogram',
-                   'idl_functions.tests.unit_test_idl_hist_equal',
-                   'idl_functions.tests.unit_test_idl_array_indices',
-                   'idl_functions.tests.unit_test_idl_bytscl',
-                   'idl_functions.tests.unit_test_idl_region_grow',
-                   'idl_functions.tests.unit_test_idl_randomu'])
+)
